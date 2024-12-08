4bd7b98a7544993e5b8f08fe1f793451d0f48fa9
James Kim <jakim929@gmail.com>
2024-12-08 13:37:38 -0800

    remove unused envvars

--- packages/frontend/src/envVars.ts
+++ packages/frontend/src/envVars.ts
@@ -1,10 +1,8 @@
-import { Address } from 'viem'
-import { z } from 'zod'
+import { Address } from "viem";
+import { z } from "zod";
 
 const envVarSchema = z.object({
   VITE_WALLET_CONNECT_PROJECT_ID: z.string().transform((x) => x as Address),
-  VITE_CROSS_DOMAIN_MESSENGER_INDEXER_URL: z.string(),
-  VITE_CONTRACT_LOOKUP_SERVICE_URL: z.string(),
-})
+});
 
-export const envVars = envVarSchema.parse(import.meta.env)
+export const envVars = envVarSchema.parse(import.meta.env);
